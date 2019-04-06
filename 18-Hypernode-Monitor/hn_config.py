644ae138152bc036a71a0a9f405d032d5e5cbf4a
geho2 <heat.gh@hovland.name>
2019-04-06 19:42:55 +0200

    Configuration file
    HN_PATH and HN_ADDRESS must be changed.

---
+++ 18-Hypernode-Monitor/hn_config.py
@@ -0,0 +1,13 @@
+"""
+Bismuth
+Configuration variables for HN monitoring script
+"""
+
+IP="127.0.0.1"
+PORT="6969"
+HN_PATH="/home/user/hypernode/modules"
+HN_ADDRESS="Your HN address starting with B..."
+TIMEOUT_1=10
+TIMEOUT_2=20
+OUTFILE_1="status.json"
+OUTFILE_2="hypernodes.json"
