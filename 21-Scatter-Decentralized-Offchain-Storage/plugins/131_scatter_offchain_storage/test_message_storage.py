c14ffa921c606e44b1ebe494fb1868c5371b23ed
EggPool <33421807+EggPool@users.noreply.github.com>
2019-06-01 19:43:50 +0200

    Formatting

--- 21-Scatter-Decentralized-Offchain-Storage/plugins/131_scatter_offchain_storage/test_message_storage.py
+++ 21-Scatter-Decentralized-Offchain-Storage/plugins/131_scatter_offchain_storage/test_message_storage.py
@@ -6,12 +6,12 @@ s = socks.socksocket()
 s.settimeout(10)
 s.connect(("127.0.0.1", 5658))
 
-connections.send (s, "SCTTR_store") #request data storage
-connections.send (s, "Hi!") #define data to store
-result = json.loads(connections.receive (s)) #receive hash of stored data
-print (result)
+connections.send(s, "SCTTR_store")  # request data storage
+connections.send(s, "Hi!")  # define data to store
+result = json.loads(connections.receive(s))  # receive hash of stored data
+print(result)
 
-connections.send (s, "SCTTR_get") #request stored data
-connections.send (s, result["hash"]) #share data hash
-reply = connections.receive (s) #receive data based on previous hash
+connections.send(s, "SCTTR_get")  # request stored data
+connections.send(s, result["hash"])  # share data hash
+reply = connections.receive(s)  # receive data based on previous hash
 print(reply)
