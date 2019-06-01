9039acdceb15ede55b1203d7dc2724b00bf2046b
EggPool <33421807+EggPool@users.noreply.github.com>
2019-06-01 19:38:36 +0200

    Formatting

--- 21-Scatter-Decentralized-Offchain-Storage/plugins/131_scatter_offchain_storage/test_file_storage.py
+++ 21-Scatter-Decentralized-Offchain-Storage/plugins/131_scatter_offchain_storage/test_file_storage.py
@@ -1,7 +1,8 @@
 import json
 import socks
-import connections
 from base64 import b85encode, b85decode
+
+import connections
 
 def decode(data, file):
     print(data)
@@ -9,7 +10,7 @@ def decode(data, file):
         outfile.write(b85decode(data))
 
 def encode(file):
-    with open(file, 'rb') as infile:
+    with open(file, "rb") as infile:
         contents = b85encode(infile.read()).decode()
         print(contents)
 
@@ -19,13 +20,13 @@ s = socks.socksocket()
 s.settimeout(10)
 s.connect(("127.0.0.1", 5658))
 
-connections.send (s, "SCTTR_store") #request data storage
-connections.send (s, encode("picture.png")) #define data to store
-result = json.loads(connections.receive (s)) #receive hash of stored data
-print ("result",result)
+connections.send(s, "SCTTR_store")  # request data storage
+connections.send(s, encode("picture.png"))  # define data to store
+result = json.loads(connections.receive (s))  # receive hash of stored data
+print("result", result)
 
-connections.send (s, "SCTTR_get") #request stored data
-connections.send (s, result["hash"]) #share data hash
-reply = connections.receive (s) #receive data based on previous hash
-print("reply",reply)
+connections.send(s, "SCTTR_get")  # request stored data
+connections.send(s, result["hash"])  # share data hash
+reply = connections.receive(s)  # receive data based on previous hash
+print("reply", reply)
 decode(reply, "test.png")
