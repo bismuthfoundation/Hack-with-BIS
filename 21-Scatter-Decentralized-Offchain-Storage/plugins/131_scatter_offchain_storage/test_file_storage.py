1127251079a8855250386981c9e200c16de84e43
HCLivess <admin@bismuth.cz>
2019-05-31 22:51:14 -0500

    correct file

--- 21-Scatter-Decentralized-Offchain-Storage/plugins/131_scatter_offchain_storage/test_file_storage.py
+++ 21-Scatter-Decentralized-Offchain-Storage/plugins/131_scatter_offchain_storage/test_file_storage.py
@@ -1,23 +1,31 @@
 import json
 import socks
 import connections
+import base64
+
+def decompress(data, file):
+    print(data)
+    with open(file, "wb") as outfile:
+        outfile.write(base64.b85decode(data))
+
+def compress(file):
+    with open(file, 'rb') as infile:
+        contents = base64.b85encode(infile.read()).decode()
+        print(contents)
+
+    return contents
 
 s = socks.socksocket()
 s.settimeout(10)
 s.connect(("127.0.0.1", 5658))
 
 connections.send (s, "SCTTR_store") #request data storage
-connections.send (s, "Hi!") #define data to store
+connections.send (s, compress("picture.png")) #define data to store
 result = json.loads(connections.receive (s)) #receive hash of stored data
-print (result)
+print ("result",result)
 
 connections.send (s, "SCTTR_get") #request stored data
 connections.send (s, result["hash"]) #share data hash
 reply = connections.receive (s) #receive data based on previous hash
-print(reply)
-
-#connections.send (s, "SCTTR_test1")
-#test1 = connections.receive (s)
-
-#connections.send (s, "SCTTR_echo2 p1 p2 p3")
-#test3 = connections.receive (s)
+print("reply",reply)
+decompress(reply, "test.png")
